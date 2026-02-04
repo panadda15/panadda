7061628b46e05334d2a477d998ef9c41a48c99e5
panadda15 <panaddasrilarak@gmail.com>
2026-02-04 16:23:22 +0700

    ฟ

--- h/check_login.php
+++ h/check_login.php
@@ -5,23 +5,3 @@
 		echo "<meta http-equiv='refresh' content='5; url=index.php'>";
 		}
 ?>
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>หน้าหลักแอดมิน - ปนัดดา</title>
-</head>
-
-<body>
-<h1>หน้าหลักแอดมิน - ปนัดดา</h1>
-
-<?php echo "แอดมิน". $_SESSION['aname']; ?><br>
- 
- <ul>
- 	<a href="products.php"><li>จัดการสินค้า</li></a>
-	<a href="products.php"><li>จัดการออเดอร์</li></a>
-    <a href="products.php"><li>จัดการลูกค้า</li></a>
-    <a href="products.php"><li>ออกจากระบบ</li></a>
-</ul>
-</body>
-</html>
--- h/index2.php
+++ h/index2.php
@@ -5,19 +5,20 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>หน้าหลักแอดมิน - ปนัดดา</title>
+<title>ปนัดดา ศรีลารักษ์ (มะปราง)</title>
 </head>
 
 <body>
-<h1>หน้าหลักแอดมิน - ปนัดดา</h1>
 
-<?php echo "แอดมิน". $_SESSION['aname']; ?><br>
- 
- <ul>
- 	<a href="products.php"><li>จัดการสินค้า</li></a>
+<h1>เข้าสู่ระบบหลังบ้าน -ปนัดดา</h1>
+
+<?php echo "แอดมิน: ". $_SESSION['a_name']; ?> <br>
+
+<ul>
+	<a href="products.php"><li>จัดการสินค้า</li></a>
 	<a href="orders.php"><li>จัดการออเดอร์</li></a>
-    <a href="costomers.php"><li>จัดการลูกค้า</li></a>
-    <a href="logout.php"><li>ออกจากระบบ</li></a>
+	<a href="customers.php"><li>จัดการลูกค้า</li></a>
+	<a href="logout.php"><li>ออกจากระบบ</li></a>
 </ul>
 </body>
 </html>
