6a424170d5e6caa2ae881c6f4cd0e25608176be0
panadda15 <panaddasrilarak@gmail.com>
2026-02-04 15:30:47 +0700

    a

--- h/index.php
+++ h/index.php
@@ -1,6 +1,3 @@
-<?php
-	session_start();
-?>
 <!doctype html>
 <html>
 <head>
@@ -9,12 +6,31 @@
 </head>
 
 <body>
-<h1>หน้าเข้าสู่ระบบหลังบ้าน - ปนัดดา</h1>
+<h1>เข้าสู่ระบบหลังบ้าน - ปนัดดา</h1>
 
 <form method ="post" action="">
 Username <input type="text" name="auser" autofocus required><br>
 Password <input type="password" name="apwd" required><br>
 <button type="submit" name="Submit">LOGIN</button>
 </form>
+
+<?php
+if(isset($_POST['Submit'])) {
+	include_once("connectdb.php");
+	$sql = "SELECT * FROM admin WHERE a_username ='{$_POST['auser']}' AND  a_password ='{$_POST['apwd']}' LIMIT 1" ;
+	$rs = mysqli_query($conn,$sql);
+	$num = mysqli_num_rows($rs);
+	
+	if ($num == 1) {
+		$data = mysqli_fetch_array($rs);
+		$_SESSION['aid'] = $data['a_id'];
+		$SESSION['aname']= $data['a_name'];
+	} else {
+		echo "<script>";
+		echo "alert('Username หรือ Password ไม่ถูกต้อง');";
+		echo "</script>";
+	}
+}
+?>
 </body>
 </html>
