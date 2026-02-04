1a21e16da726a2df01e177d0635bba5700012b43
panadda15 <panaddasrilarak@gmail.com>
2026-02-04 16:39:26 +0700

    a

--- h/password_hack.php
+++ h/password_hack.php
@@ -1,7 +1,39 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ปนัดดา ศรีลารักษ์ (มะปราง)</title>
+</head>
+
+<body>
+<h1>เข้าสู่ระบบหลังบ้าน - ปนัดดา</h1>
+
+<form method ="post" action="">
+Username <input type="text" name="auser" autofocus required><br>
+Password <input type="password" name="apwd" required><br>
+<button type="submit" name="Submit">LOGIN</button>
+</form>
+
 <?php
-	session_start();
-	if(empty($_SESSION['aid'])) {
-		echo "Access Denied";
-		echo "<meta http-equiv='refresh' content='5; url=index.php'>";
-		}
+if(isset($_POST['Submit'])) {
+	include_once("connectdb.php");
+	$sql = "SELECT * FROM admin WHERE a_username ='{$_POST['auser']}' AND  a_password ='{$_POST['apwd']}' LIMIT 1" ;
+	$rs = mysqli_query($conn,$sql);
+	$num = mysqli_num_rows($rs);
+	
+	if ($num == 1) {
+		$data = mysqli_fetch_array($rs);
+		$_SESSION['aid'] = $data['a_id'];
+		$SESSION['aname']= $data['a_name'];
+		echo "<script>";
+		echo "window.location='index2.php';";
+		echo "</script>";
+	} else {
+		echo "<script>";
+		echo "alert('Username หรือ Password ไม่ถูกต้อง');";
+		echo "</script>";
+	}
+}
 ?>
+</body>
+</html>
