c85fdf7633c04553161be41455ccacd4e6e89068
panadda15 <panaddasrilarak@gmail.com>
2026-02-04 13:53:28 +0700

    a

--- e/connectdb.php
+++ e/connectdb.php
@@ -1,7 +1,7 @@
 <?php
      $host = "localhost";
      $user = "root";
-     $pwd = "";
+     $pwd = "66010914005";
      $db = "4050db";
      $conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
      mysqli_query ($conn, "SET NAMES utf8");
