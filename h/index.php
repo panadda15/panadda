4feb7dd17b662a02658ffee5ab9a20575307ceab
panadda15 <panaddasrilarak@gmail.com>
2026-02-04 14:41:08 +0700

    a

---
+++ h/index.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ปนัดดา ศรีลารักษ์ (มะปราง)</title>
+</head>
+
+<body>
+<h1>หน้าเข้าสู่ระบบ = ปนัดดา</h1>
+
+<form method ="post" action="">
+Username <input type="text" name="auser" autofocus required><br>
+Password <input type="password" name="apwd" required><br>
+<button type="submit" name="Submit">LOGIN</button>
+</form>
+</body>
+</html>
