11ba236a56aa847164c30cfb8d2ad705b2459af2
panadda15 <panaddasrilarak@gmail.com>
2026-02-17 18:45:38 +0700

    1

--- i/a.php
+++ i/a.php
@@ -22,7 +22,7 @@ $rs = mysqli_query($conn, $sql);
     <tr>
     	<td><?php echo $data['r_id'] ; ?></td>
         <td><?php echo $data['r_name'] ;?></td>
-        <td width="80" align="center"><img src="image/delete.jpg" width="20"></td>
+        <td width="80" align="center"><img src="images/delete.jpg" width="20"></td>
     </tr>
 <?php } ?>
 </table>
