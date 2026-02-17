90983a7e3d0ad5da660de005e67902f818d265de
panadda15 <panaddasrilarak@gmail.com>
2026-02-17 21:08:31 +0700

    a

--- i/b.php
+++ i/b.php
@@ -62,8 +62,8 @@ $rs = mysqli_query($conn, $sql);
     <tr>
     	<td><?php echo $data['p_id'] ; ?></td>
         <td><?php echo $data['p_name'] ;?></td>
-        <td width="80" align="center"><img src="images/<?php echo $data['p_id'] ; ?>.<?php echo $data['p_ext'] ; ?>" width="50"></a></td>
-        <td width="80" align="center"><a href="delete_provinces.php?id=<?php echo $data['r_id']; ?>" onClick="return confirm('d1');"><img src="images/delete.jpg" width="50"></a></td>
+        <td width="80" align="center"><img src="images/<?php echo $data['p_id'] ; ?>.<?php echo $data['p_ext'] ; ?>" width="140"></a></td>
+        <td width="80" align="center"><a href="delete_provinces.php?id=<?php echo $data['r_id']; ?>" onClick="return confirm('d1');"><img src="images/delete.jpg" width="140"></a></td>
     </tr>
 <?php } ?>
 </table>
