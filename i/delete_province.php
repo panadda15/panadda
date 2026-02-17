0a7cce8a4ca9e535bdb50060126b2f1f276e263c
panadda15 <panaddasrilarak@gmail.com>
2026-02-17 21:24:37 +0700

    a

--- i/delete_province.php
+++ i/delete_province.php
@@ -4,7 +4,7 @@ include_once("connectdb.php");
 
 $id = $_GET['id'];
 $ext = $_GET['ext'];
-$sql = "DELETE FROM provinces WHERE p_id='{$id}' ";
+$sql = "DELETE FROM province WHERE p_id='{$id}' ";
 mysqli_query($conn, $sql) or die ("ลบข้อมูลไม่ได้");
 
 unlink("images/".$id.".".$ext);
