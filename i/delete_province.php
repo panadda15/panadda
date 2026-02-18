2c1b15a44ec74c7f2ecc910279839db01e3e312a
panadda14 <panaddasrilarak@gmail.com>
2026-02-18 14:27:59 +0700

    a

--- i/delete_province.php
+++ i/delete_province.php
@@ -2,16 +2,25 @@
 <?php
 include_once("connectdb.php");
 
-$id = $_GET['id'];
-$ext = $_GET['ext'];
-$sql = "DELETE FROM provinces WHERE p_id='{$id}' ";
-mysqli_query($conn, $sql) or die ("ลบข้อมูลไม่ได้");
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $ext = $_GET['ext']; 
 
-unlink("images/".$id.".".$ext);
+    $sql_del = "DELETE FROM provinces WHERE p_id = '{$id}'";
+    mysqli_query($conn, $sql_del) or die ("ลบข้อมูลไม่ได้: " . mysqli_error($conn));
 
+    if($ext != ""){
+        $file_path = "images/" . $id . "." . $ext;
+        if(file_exists($file_path)){
+            unlink($file_path);
+        }
+    }
 
-echo "<script>";
-echo "window.location='b.php';";
-echo "</script>";
-
+    echo "<script>";
+    echo "alert('ลบข้อมูลเรียบร้อยแล้ว');";
+    echo "window.location='b.php';"; 
+    echo "</script>";
+} else {
+    echo "<script>window.location='b.php';</script>";
+}
 ?>
