1c02455b55fdc66e47f995e989db48348d95b270
panadda14 <panaddasrilarak@gmail.com>
2026-02-18 16:52:01 +0700

    ฟ

--- k/a.php
+++ k/a.php
@@ -7,6 +7,7 @@
 
 <body>
 <h1>งาน k 66010914050 ปนัดดา ศรีลารักษ์ (มะปราง)</h1>
-
+<button type="submit" name="Submit">ปุ่มที่1</button>
+<button type="submit" name="Submit">ปุ่มที่2</button>
 </body>
 </html>
