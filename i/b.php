17640f03a17d8bad2143e023329943700f905886
panadda14 <panaddasrilarak@gmail.com>
2026-02-18 16:35:42 +0700

    a

--- i/b.php
+++ i/b.php
@@ -77,7 +77,7 @@ while ($data = mysqli_fetch_array($rs_show)){
         
         <td align="center">
             <a href="delete_province.php?id=<?php echo $data['p_id']; ?>&ext=<?php echo $data['p_ext']; ?>" onClick="return confirm('ยืนยันการลบข้อมูลนี้?');">
-                <img src="images/D.jpg" width="30">
+                <img src="images/D.jpg" width="50">
                 
                 </a>
         </td>
